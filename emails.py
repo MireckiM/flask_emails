14ebe49004bea7b95350bbfba79177b698dcd7a2
Marcin Mirecki <marcin.mirecki@poleng.pl>
2023-10-04 15:30:21 +0200

    optim

--- emails.py
+++ emails.py
@@ -16,19 +16,7 @@ def clean(text):
     # clean text for creating a folder
     return "".join(c if c.isalnum() else "_" for c in text)
 
-#class mail:
-#    def __init__(self, subject, sender, content, filename, file_data, analysis):
-#        self.subject = subject
-#        self.sender = sender
-#        self.content = content
-#        self.filename = filename
-#        self.file_data = file_data
-#        self.analysis = analysis
-#
-#    def __str__(self):
-#        return f'Temat: {self.subject}, Nadawca: {self.sender}, Treść: {self.content}, Nazwa plku: {self.filename}, Plik: {self.file_data}, Analiza: {self.analysis}'
 
-#mailbox=[]
 email_data = []
 
 configure()
@@ -71,7 +59,6 @@ def getEmails():
                     
                 email_item["subject"] = subject
                 email_item["from"] = From
-                #email_item["attachments"]
                 print("Subject:", subject)
                 print("From:", From)
                 # if the email message is multipart
@@ -91,9 +78,6 @@ def getEmails():
                             print(body)
                             email_item["body"] = body
                             email_item["analysis"] = _analyser.analyseMail(body)
-                            #print(_analyser.analyseMail(body))
-                            #mailbox.append(mail(subject,From,body,"", _analyser.analyseMail(body)))
-                            #print(mailbox[0])
                             filename = None
                             content_type = None
                             file_data = None
@@ -109,9 +93,7 @@ def getEmails():
                                 filename = decode_attachment_name(filename)
                                 attachment_data = part.get_payload(decode=True)
                                 folder_name = clean(subject)
-                            #    if not os.path.isdir(folder_name):
-                            #        # make a folder for this email (named after the subject)
-                            #        os.mkdir(folder_name)
+                           
                                 filepath = os.path.join(folder_name, filename)
                                 attachment_info = {
                                     "filename": filename,
@@ -119,9 +101,8 @@ def getEmails():
                                     "filepath": filepath
                                 }
                                 email_item["attachments"].append(attachment_info)
-                            #    # download attachment and save it
-                            #    open(filepath, "wb").write(part.get_payload(decode=True))
-                            #    webbrowser.open(filepath)
+                            
+                            
 
                 else:
                     # extract content type of email
@@ -163,5 +144,72 @@ def save_attachment(file_data, filename, download_folder):
     with open(file_path, "wb") as file:
         file.write(file_data)
     return file_path
+
+def download(attachmentName):
+    imap_server = "imap.dpoczta.pl"
+    # create an IMAP4 class with SSL
+    imap = imaplib.IMAP4_SSL(imap_server)
+    # authenticate
+    imap.login(os.getenv('username'), os.getenv('password'))
+    status, messages = imap.select("INBOX")
+    N = 3
+    # total number of emails
+    messages = int(messages[0])
+    for i in range(messages, messages-N, -1):
+        email_item = {}
+        email_item["attachments"] = []
+        # fetch the email message by ID
+        res, msg = imap.fetch(str(i), "(RFC822)")
+        for response in msg:
+            if isinstance(response, tuple):
+                # parse a bytes email into a message object
+                msg = email.message_from_bytes(response[1])
+                # decode the email subject
+                subject, encoding = decode_header(msg["Subject"])[0]
+                if isinstance(subject, bytes):
+                    # if it's a bytes, decode to str
+                    subject = subject.decode(encoding)
+                #    decode email sender
+                From, encoding = decode_header(msg.get("From"))[0]
+                if isinstance(From, bytes):
+                    From = From.decode(encoding)
+                    
+                # if the email message is multipart
+                if msg.is_multipart():
+                    # iterate over email parts
+                    for part in msg.walk():
+                        # extract content type of email
+                        content_type = part.get_content_type()
+                        content_disposition = str(part.get("Content-Disposition"))
+                        try:
+                            # get the email body
+                            body = part.get_payload(decode=True).decode()
+                        except:
+                            pass
+                        if content_type == "text/plain" and "attachment" not in content_disposition:
+                            pass
+
+                        elif "attachment" in content_disposition:
+                            file_data = part.get_payload(decode=True)
+                                                       
+                            # download attachment
+                            filename = part.get_filename()
+                            filename = decode_attachment_name(filename)
+                          
+
+                            if filename == attachmentName:
+                                folder_name = clean(subject)
+                                if not os.path.isdir(folder_name):
+                                    # make a folder for this email (named after the subject)
+                                    os.mkdir(folder_name)
+                                filepath = os.path.join(folder_name, filename)
+                                # download attachment and save it
+                                open(filepath, "wb").write(part.get_payload(decode=True))
+                                break
+                            else:
+                                pass
+
+    print("downloading " + attachmentName)
+    return f'Plik {filename} zostanie zapisany w {filepath}'
 
 
